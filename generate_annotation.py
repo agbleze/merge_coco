b9789125edc1fa5e12aaf552e9021e2e39d240e3
linus <lagbleze@st.ug.edu.gh>
2024-04-06 23:21:29 +0200

    add generate

---
+++ generate_annotation.py
@@ -0,0 +1,13 @@
+
+
+def generate_duplicate_annotation():
+    annotation = {"images": [{"file_name": "image_1.jpg",
+                 "id": 1,
+                 "heigth": 600,
+                 "width": 600
+                 }],
+     "annotations": [],
+     "categories": []
+     }
+    
+    return annotation
---
+++ sample.py
@@ -0,0 +1,4 @@
+
+#%%
+print("rising hope test case")
+# %%
